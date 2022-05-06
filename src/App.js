9b670dd1ef45df4a8002fddc7070c430738e035a
Avinash Kumar <ak2449746@gmail.com>
2022-05-07 01:56:23 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useCallback } from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import './App.css';
+import particlesOptions from "./particles.json";
+
+function App() {
+    const particlesInit = useCallback(main => {
+        loadFull(main);
+    }, [])
+
+    return (
+        <div className="App">
+            <Particles options={particlesOptions} init={particlesInit} />
+            <header className="App-header">
+                Hello World
+            </header>
+
+        </div>
+    );
+}
+
+export default App;
